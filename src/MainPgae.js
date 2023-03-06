b6e5b2e093e99e87a2d7b1b687633e5c1a292085
harishpalsande9 <harishpalsande9@gmail.com>
2023-03-07 01:04:17 +0530

    MainPgae

---
+++ src/MainPgae.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MainPgae() {
+  return (
+    <div>
+      <p>MainPgae</p>
+      <p>MainPgae</p>
+      <p>MainPgae</p>
+      <p>MainPgae</p>
+    </div>
+  );
+}
+
+export default MainPgae;
